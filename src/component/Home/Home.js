50f0f8ac64cbb302727004a65c0020df0535e7dd
Imran Hossen <freelancerimran81@gmail.com>
2022-07-20 17:50:54 +0600

    PopularAritcle section done

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import PopularArticle from '../PopularArticle/PopularArticle';
 
 const Home = () => {
     return (
         <div>
             <Banner/>
+            <PopularArticle/>
         </div>
     );
 };
