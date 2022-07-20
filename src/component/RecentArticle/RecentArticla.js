e27d0c4d69f6cd2895a87aa57677ecd4bee6b5d6
Imran Hossen <freelancerimran81@gmail.com>
2022-07-21 00:53:14 +0600

    recent article updated

--- src/component/RecentArticle/RecentArticla.js
+++ src/component/RecentArticle/RecentArticla.js
@@ -13,21 +13,21 @@ const RecentArticla = () => {
 
                 <div className=" bg-neutral shadow-xl rounded-2xl">
                     <figure><img className='w-full rounded-t' src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
-                    <div className="card-body">
+                    <div className="card-body p-5">
                         <h2 className="text-warning text-sm">
                             {year}
                         </h2>
-                        <h1 className='text-2xl font-bold text-warning'>Child's Sensory and Motor, Development</h1>
+                        <h1 className='text-xl font-bold text-warning'>Child's Sensory and Motor, Development</h1>
                         <p className='text-sm text-warning'>On the other hand, we denounce with righteous indignation and dislike men who are so beguiled.</p>
 
-                        <div className="flex items-center gap-5 mt-5">
+                        <div className="flex items-center gap-5 mt-2">
                             <div className="avatar ">
-                                <div className="w-20 mt-2 rounded-full ring ring-primary ring-offset-base-100 ">
+                                <div className="w-14 mt-2 rounded-full ring ring-primary ring-offset-base-100 ">
                                     <img src="https://placeimg.com/192/192/people" alt='' />
                                 </div>
                             </div>
                             <div className=" text-warning">
-                                <h1 className='font-bold text-xl'>Armin Sultana</h1>
+                                <h1 className='font-bold '>Armin Sultana</h1>
                                 <h1 className='text-sm'>Study power CEO</h1>
                             </div>
                         </div>
@@ -35,21 +35,21 @@ const RecentArticla = () => {
                 </div>
                 <div className=" bg-neutral shadow-xl rounded-2xl">
                     <figure><img className='w-full rounded-t' src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
-                    <div className="card-body">
+                    <div className="card-body p-5">
                         <h2 className="text-warning text-sm">
                             {year}
                         </h2>
-                        <h1 className='text-2xl font-bold text-warning'>Child's Sensory and Motor, Development</h1>
+                        <h1 className='text-xl font-bold text-warning'>Child's Sensory and Motor, Development</h1>
                         <p className='text-sm text-warning'>On the other hand, we denounce with righteous indignation and dislike men who are so beguiled.</p>
 
-                        <div className="flex items-center gap-5 mt-5">
+                        <div className="flex items-center gap-5 mt-2">
                             <div className="avatar ">
-                                <div className="w-20 mt-2 rounded-full ring ring-primary ring-offset-base-100 ">
+                                <div className="w-14 mt-2 rounded-full ring ring-primary ring-offset-base-100 ">
                                     <img src="https://placeimg.com/192/192/people" alt='' />
                                 </div>
                             </div>
                             <div className=" text-warning">
-                                <h1 className='font-bold text-xl'>Armin Sultana</h1>
+                                <h1 className='font-bold '>Armin Sultana</h1>
                                 <h1 className='text-sm'>Study power CEO</h1>
                             </div>
                         </div>
@@ -57,21 +57,21 @@ const RecentArticla = () => {
                 </div>
                 <div className=" bg-neutral shadow-xl rounded-2xl">
                     <figure><img className='w-full rounded-t' src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
-                    <div className="card-body">
+                    <div className="card-body p-5">
                         <h2 className="text-warning text-sm">
                             {year}
                         </h2>
-                        <h1 className='text-2xl font-bold text-warning'>Child's Sensory and Motor, Development</h1>
+                        <h1 className='text-xl font-bold text-warning'>Child's Sensory and Motor, Development</h1>
                         <p className='text-sm text-warning'>On the other hand, we denounce with righteous indignation and dislike men who are so beguiled.</p>
 
-                        <div className="flex items-center gap-5 mt-5">
+                        <div className="flex items-center gap-5 mt-2">
                             <div className="avatar ">
-                                <div className="w-20 mt-2 rounded-full ring ring-primary ring-offset-base-100 ">
+                                <div className="w-14 mt-2 rounded-full ring ring-primary ring-offset-base-100 ">
                                     <img src="https://placeimg.com/192/192/people" alt='' />
                                 </div>
                             </div>
                             <div className=" text-warning">
-                                <h1 className='font-bold text-xl'>Armin Sultana</h1>
+                                <h1 className='font-bold '>Armin Sultana</h1>
                                 <h1 className='text-sm'>Study power CEO</h1>
                             </div>
                         </div>
