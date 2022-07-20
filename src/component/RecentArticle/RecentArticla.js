d7ac1dc80c1df5d2834a444b78c28917a70079a5
Imran Hossen <freelancerimran81@gmail.com>
2022-07-20 19:29:05 +0600

    RecentArticle updated

--- src/component/RecentArticle/RecentArticla.js
+++ src/component/RecentArticle/RecentArticla.js
@@ -6,76 +6,78 @@ const RecentArticla = () => {
     return (
         <section className='mid-container'>
             <div className="">
-                <h1 className='text-5xl mt-24'>Recent Article</h1>
+                <h1 className='text-3xl font-bold mt-24'>Recent Article</h1>
                 <p className='mt-2'>Recent Article are selected based on the number of readers</p>
             </div>
-            <div className="grid grid-cols-3 mt-10 gap-0  ">
-                <div class="  bg-base-100 shadow-xl w-[370px] ">
-                    <figure><img className='w-full h-[267px]' src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
-                    <div class="card-body">
-                        <h2 class="">
-                            {year}
+            <div className="grid grid-cols-3 mt-10 gap-5 ">
 
+                <div class=" bg-neutral shadow-xl rounded-2xl">
+                    <figure><img className='w-full rounded-t' src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+                    <div class="card-body">
+                        <h2 class="text-warning text-sm">
+                            {year}
                         </h2>
-                        <h1 className='text-xl font-bold'>Child's Sensory and Motor, Developmentt</h1>
-                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est perferendis ipsam aliquam.</p>
-                    </div>
-                    <div className="flex  items-center ml-6 ">
-                    <div class="avatar ">
-                        <div class="w-[36px] h-[36px] mt-2 rounded-full">
-                            <img src="https://placeimg.com/192/192/people" />
+                        <h1 className='text-2xl font-bold text-warning'>Child's Sensory and Motor, Development</h1>
+                        <p className='text-sm text-warning'>On the other hand, we denounce with righteous indignation and dislike men who are so beguiled.</p>
+
+                        <div className="flex items-center gap-5 mt-5">
+                            <div class="avatar ">
+                                <div class="w-20 mt-2 rounded-full ring ring-primary ring-offset-base-100 ">
+                                    <img src="https://placeimg.com/192/192/people" alt='' />
+                                </div>
+                            </div>
+                            <div className=" text-warning">
+                                <h1 className='font-bold text-xl'>Armin Sultana</h1>
+                                <h1 className='text-sm'>Study power CEO</h1>
+                            </div>
                         </div>
-                        <div className="ml-2">
-                            <h1 className='font-bold'>Armin Sultana</h1>
-                            <h1 className='text-small'>Study power CEO</h1>
-                        </div>
-                    </div>
                     </div>
                 </div>
-                <div class=" bg-base-100 shadow-xl w-[370px]">
-                    <figure><img className='w-full h-[267px]' src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+                <div class=" bg-neutral shadow-xl rounded-2xl">
+                    <figure><img className='w-full rounded-t' src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
                     <div class="card-body">
-                        <h2 class="">
+                        <h2 class="text-warning text-sm">
                             {year}
-
                         </h2>
-                        <h1 className='text-xl font-bold'>Master Buisness Class English for your buisness</h1>
-                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est perferendis ipsam aliquam.</p>
-                    </div>
-                    <div className="flex  items-center ml-6 ">
-                    <div class="avatar ">
-                        <div class="w-[36px] h-[36px] mt-2 rounded-full">
-                            <img src="https://placeimg.com/192/192/people" />
+                        <h1 className='text-2xl font-bold text-warning'>Child's Sensory and Motor, Development</h1>
+                        <p className='text-sm text-warning'>On the other hand, we denounce with righteous indignation and dislike men who are so beguiled.</p>
+
+                        <div className="flex items-center gap-5 mt-5">
+                            <div class="avatar ">
+                                <div class="w-20 mt-2 rounded-full ring ring-primary ring-offset-base-100 ">
+                                    <img src="https://placeimg.com/192/192/people" alt='' />
+                                </div>
+                            </div>
+                            <div className=" text-warning">
+                                <h1 className='font-bold text-xl'>Armin Sultana</h1>
+                                <h1 className='text-sm'>Study power CEO</h1>
+                            </div>
                         </div>
-                        <div className="ml-2">
-                            <h1 className='font-bold'>Arifa Akter Chaity</h1>
-                            <h1 className='text-small'>Study power CEO</h1>
-                        </div>
-                    </div>
                     </div>
                 </div>
-                <div class="  bg-base-100 shadow-xl w-[370px]">
-                    <figure><img className='w-full h-[267px]' src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+                <div class=" bg-neutral shadow-xl rounded-2xl">
+                    <figure><img className='w-full rounded-t' src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
                     <div class="card-body">
-                        <h2 class="">
+                        <h2 class="text-warning text-sm">
                             {year}
-
                         </h2>
-                        <h1 className='text-xl font-bold'>Algorima Math aljabar,geometri aritmatika</h1>
-                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est perferendis ipsam aliquam.</p>
-                    </div>
-                    <div className="flex  items-center ml-6 ">
-                    <div class="avatar ">
-                        <div class="w-[36px] h-[36px] mt-2 rounded-full">
-                            <img src="https://placeimg.com/192/192/people" />
+                        <h1 className='text-2xl font-bold text-warning'>Child's Sensory and Motor, Development</h1>
+                        <p className='text-sm text-warning'>On the other hand, we denounce with righteous indignation and dislike men who are so beguiled.</p>
+
+                        <div className="flex items-center gap-5 mt-5">
+                            <div class="avatar ">
+                                <div class="w-20 mt-2 rounded-full ring ring-primary ring-offset-base-100 ">
+                                    <img src="https://placeimg.com/192/192/people" alt='' />
+                                </div>
+                            </div>
+                            <div className=" text-warning">
+                                <h1 className='font-bold text-xl'>Armin Sultana</h1>
+                                <h1 className='text-sm'>Study power CEO</h1>
+                            </div>
                         </div>
-                        <div className="ml-2">
-                            <h1 className='font-bold'>Imran Hussain</h1>
-                            <h1 className='text-small'>Study power CEO</h1>
-                        </div>
-                    </div>
                     </div>
                 </div>
+
             </div>
         </section>
     );
