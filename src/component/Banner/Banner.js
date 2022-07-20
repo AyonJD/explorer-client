3aa571eb2d9a950545a2a2e5fdc39c2db711f51e
Imran Hossen <freelancerimran81@gmail.com>
2022-07-20 15:32:44 +0600

    updated banner section

--- src/component/Banner/Banner.js
+++ src/component/Banner/Banner.js
@@ -3,12 +3,12 @@ import img from "../../assets/banner-img/bored.png"
 
 const Banner = () => {
     return (
-        <section className='mid-container relative banner absolute '>
+        <section className='mid-container relative banner'>
             <div className=' grid lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1'>
                 <div className='flex justify-center items-center'>
                     <div>
                         <span className='bg-neutral p-2 rounded-2xl text-warning'><button class="btn btn-xs btn-primary">Tiny</button> Becoming a new program online</span>
-                        <h1 className='text-5xl font-bold my-5'>Find the Best Article
+                        <h1 className='text-5xl font-bold my-5 '>Find the Best Article
                             Online for your Thesis</h1>
                     </div>
                 </div>
@@ -16,22 +16,22 @@ const Banner = () => {
                     <img src={img} alt="" />
                 </div>
             </div>
-            <div class="card lg:w-1/4 bg-neutral shadow-xl p-10 absolute lg:bottom-[-50px] bottom-[-180px] border-t-8 border-primary border">
-                <div className='grid grid-cols-2 gap-5 text-warning font-semibold'>
+            <div class="card lg:w-1/4 bg-neutral shadow-xl p-10 absolute lg:bottom-[-50px] md:bottom-[-120px] bottom-[-180px] border-t-8 border-primary border">
+                <div className='grid grid-cols-2 gap-5 text-warning '>
                     <div className='text-center'>
-                        <h2 className='text-3xl'>2000+</h2>
+                        <h2 className='text-3xl font-semibold'>2000+</h2>
                         <h2>Articles Submitted</h2>
                     </div>
-                    <div className='text-center'>
-                        <h2 className='text-3xl'>150</h2>
+                    <div className='text-center '>
+                        <h2 className='text-3xl font-semibold'>150</h2>
                         <h2>Category Listed</h2>
                     </div>
                     <div className='text-center'>
-                        <h2 className='text-3xl'>300+</h2>
+                        <h2 className='text-3xl font-semibold'>300+</h2>
                         <h2>Articles Contributors</h2>
                     </div>
                     <div className='text-center'>
-                        <h2 className='text-3xl'>90%</h2>
+                        <h2 className='text-3xl font-semibold'>90%</h2>
                         <h2>Satisfaction Rate</h2>
                     </div>
                 </div>
