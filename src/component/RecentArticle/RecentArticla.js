054d4d942c47f56940d7d7e56bb8393b814e1727
Imran Hossen <freelancerimran81@gmail.com>
2022-07-21 01:14:55 +0600

    popular article updated

--- src/component/RecentArticle/RecentArticla.js
+++ src/component/RecentArticle/RecentArticla.js
@@ -11,18 +11,18 @@ const RecentArticla = () => {
             </div>
             <div className="grid grid-cols-3 mt-10 gap-5 ">
 
-                <div className=" bg-neutral shadow-xl rounded-2xl">
+                <div className=" bg-neutral shadow-xl rounded-xl">
                     <figure><img className='w-full rounded-t' src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
                     <div className="card-body p-5">
-                        <h2 className="text-warning text-sm">
+                        <p className="text-warning text-sm">
                             {year}
-                        </h2>
+                        </p>
                         <h1 className='text-xl font-bold text-warning'>Child's Sensory and Motor, Development</h1>
                         <p className='text-sm text-warning'>On the other hand, we denounce with righteous indignation and dislike men who are so beguiled.</p>
 
                         <div className="flex items-center gap-5 mt-2">
                             <div className="avatar ">
-                                <div className="w-14 mt-2 rounded-full ring ring-primary ring-offset-base-100 ">
+                                <div className="w-12 mt-2 rounded-full ring ring-primary ring-offset-base-100 ">
                                     <img src="https://placeimg.com/192/192/people" alt='' />
                                 </div>
                             </div>
@@ -33,7 +33,7 @@ const RecentArticla = () => {
                         </div>
                     </div>
                 </div>
-                <div className=" bg-neutral shadow-xl rounded-2xl">
+                <div className=" bg-neutral shadow-xl rounded-xl">
                     <figure><img className='w-full rounded-t' src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
                     <div className="card-body p-5">
                         <h2 className="text-warning text-sm">
@@ -44,7 +44,7 @@ const RecentArticla = () => {
 
                         <div className="flex items-center gap-5 mt-2">
                             <div className="avatar ">
-                                <div className="w-14 mt-2 rounded-full ring ring-primary ring-offset-base-100 ">
+                                <div className="w-12 mt-2 rounded-full ring ring-primary ring-offset-base-100 ">
                                     <img src="https://placeimg.com/192/192/people" alt='' />
                                 </div>
                             </div>
@@ -55,7 +55,7 @@ const RecentArticla = () => {
                         </div>
                     </div>
                 </div>
-                <div className=" bg-neutral shadow-xl rounded-2xl">
+                <div className=" bg-neutral shadow-xl rounded-xl">
                     <figure><img className='w-full rounded-t' src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
                     <div className="card-body p-5">
                         <h2 className="text-warning text-sm">
@@ -66,7 +66,7 @@ const RecentArticla = () => {
 
                         <div className="flex items-center gap-5 mt-2">
                             <div className="avatar ">
-                                <div className="w-14 mt-2 rounded-full ring ring-primary ring-offset-base-100 ">
+                                <div className="w-12 mt-2 rounded-full ring ring-primary ring-offset-base-100 ">
                                     <img src="https://placeimg.com/192/192/people" alt='' />
                                 </div>
                             </div>
