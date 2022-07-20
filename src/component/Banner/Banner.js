43c9a508344e1c45659017d801c286e924acd57e
Imran Hossen <freelancerimran81@gmail.com>
2022-07-20 23:00:35 +0600

    Banner section updated

--- src/component/Banner/Banner.js
+++ src/component/Banner/Banner.js
@@ -7,7 +7,7 @@ const Banner = () => {
             <div className=' grid lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1 border-b-4 border-indigo-500'>
                 <div className='flex justify-center items-center'>
                     <div>
-                        <span className='bg-neutral p-2 rounded-2xl text-warning'><button class="btn btn-xs btn-primary">Tiny</button> Becoming a new program online</span>
+                        <span className='bg-neutral p-2 rounded-2xl text-warning'><button class="btn btn-xs btn-primary">New</button> Becoming a new program online</span>
                         <h1 className='text-5xl font-bold my-5 '>Find the Best Article
                             Online for your Thesis</h1>
                     </div>
