85910793a83bd238ea8cd07eba816e3c89730c03
Armin Sultana <arminsultana556@gmail.com>
2022-07-21 11:11:30 +0600

    footer commites

--- src/component/RecentArticle/RecentArticla.js
+++ src/component/RecentArticle/RecentArticla.js
@@ -12,31 +12,18 @@ const RecentArticla = () => {
             <p className='text-sm mt-2'>Popular articles are selected based on the number of readers</p>
             <div className="grid grid-cols-3 mt-10 gap-5 ">
 
-<<<<<<< HEAD
-                <div class=" bg-neutral shadow-xl ">
-                    <figure><img className='w-full rounded-t' src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
-                    <div class="card-body">
-                        <h2 class="text-warning text-sm">
-=======
                 <div className=" bg-neutral shadow-xl rounded">
                     <figure><img className='w-full rounded-t' src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
                     <div className="card-body p-5">
                         <p className="text-warning text-sm">
->>>>>>> 602bc051ed92e32b5a87d37a3570e93a79a7c285
                             {year}
                         </p>
                         <h1 className='text-xl font-bold text-warning'>Child's Sensory and Motor, Development</h1>
                         <p className='text-sm text-warning'>On the other hand, we denounce with righteous indignation and dislike men who are so beguiled.</p>
 
-<<<<<<< HEAD
-                        <div className="flex items-center gap-5 mt-5">
-                            <div class="avatar ">
-                                <div class="w-20 mt-2 rounded-full ring ring-primary ring-offset-base-100 ">
-=======
                         <div className="flex items-center gap-5 mt-2">
                             <div className="avatar ">
                                 <div className="w-12 mt-2 rounded-full ring ring-primary ring-offset-base-100 ">
->>>>>>> 602bc051ed92e32b5a87d37a3570e93a79a7c285
                                     <img src="https://placeimg.com/192/192/people" alt='' />
                                 </div>
                             </div>
@@ -47,31 +34,18 @@ const RecentArticla = () => {
                         </div>
                     </div>
                 </div>
-<<<<<<< HEAD
-                <div class=" bg-neutral shadow-xl ">
-                    <figure><img className='w-full rounded-t' src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
-                    <div class="card-body">
-                        <h2 class="text-warning text-sm">
-=======
                 <div className=" bg-neutral shadow-xl rounded">
                     <figure><img className='w-full rounded-t' src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
                     <div className="card-body p-5">
                         <h2 className="text-warning text-sm">
->>>>>>> 602bc051ed92e32b5a87d37a3570e93a79a7c285
                             {year}
                         </h2>
                         <h1 className='text-xl font-bold text-warning'>Child's Sensory and Motor, Development</h1>
                         <p className='text-sm text-warning'>On the other hand, we denounce with righteous indignation and dislike men who are so beguiled.</p>
 
-<<<<<<< HEAD
-                        <div className="flex items-center gap-5 mt-5">
-                            <div class="avatar ">
-                                <div class="w-20 mt-2 rounded-full ring ring-primary ring-offset-base-100 ">
-=======
                         <div className="flex items-center gap-5 mt-2">
                             <div className="avatar ">
                                 <div className="w-12 mt-2 rounded-full ring ring-primary ring-offset-base-100 ">
->>>>>>> 602bc051ed92e32b5a87d37a3570e93a79a7c285
                                     <img src="https://placeimg.com/192/192/people" alt='' />
                                 </div>
                             </div>
@@ -82,31 +56,18 @@ const RecentArticla = () => {
                         </div>
                     </div>
                 </div>
-<<<<<<< HEAD
-                <div class=" bg-neutral shadow-xl ">
-                    <figure><img className='w-full rounded-t' src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
-                    <div class="card-body">
-                        <h2 class="text-warning text-sm">
-=======
                 <div className=" bg-neutral shadow-xl rounded">
                     <figure><img className='w-full rounded-t' src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
                     <div className="card-body p-5">
                         <h2 className="text-warning text-sm">
->>>>>>> 602bc051ed92e32b5a87d37a3570e93a79a7c285
                             {year}
                         </h2>
                         <h1 className='text-xl font-bold text-warning'>Child's Sensory and Motor, Development</h1>
                         <p className='text-sm text-warning'>On the other hand, we denounce with righteous indignation and dislike men who are so beguiled.</p>
 
-<<<<<<< HEAD
-                        <div className="flex items-center gap-5 mt-5">
-                            <div class="avatar ">
-                                <div class="w-20 mt-2 rounded-full ring ring-primary ring-offset-base-100 ">
-=======
                         <div className="flex items-center gap-5 mt-2">
                             <div className="avatar ">
                                 <div className="w-12 mt-2 rounded-full ring ring-primary ring-offset-base-100 ">
->>>>>>> 602bc051ed92e32b5a87d37a3570e93a79a7c285
                                     <img src="https://placeimg.com/192/192/people" alt='' />
                                 </div>
                             </div>
