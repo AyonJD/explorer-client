1ff87677a0bdffd86efeb460fa87d8782d9d37af
Armin Sultana <arminsultana556@gmail.com>
2022-07-26 13:02:10 +0600

    popular section updated

--- src/component/PopularArticle/PopularArticle.js
+++ src/component/PopularArticle/PopularArticle.js
@@ -41,7 +41,7 @@ const PopularArticle = () => {
                     </div>
                 </div>
                 <div className="">
-                    <div class="card w-52 h-full bg-base-100 shadow-xl ml-10  ">
+                    <div class="card w-52  bg-base-100 shadow-xl ml-10  ">
                         <div class="card-body ">
                             <h2 className='font-bold text-center'>. All Category</h2>
                             <ul className='pl-5 text-sm'>
@@ -56,6 +56,46 @@ const PopularArticle = () => {
                                 <li className='flex'> <span className='mt-1'><RiArrowRightSFill></RiArrowRightSFill></span>Entertainment</li>
                           
                             </ul>
+                            
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <div className='mt-10 flex w-full'>
+
+                <div class="hero border rounded-2xl w-2/3 ">
+                    <div class="hero-content grid lg:grid-cols-2 md:grid-cols-2 ">
+                        <div><img src={img1} class="lg:max-w-md md:max-w-sm w-full  rounded-lg shadow-2xl" alt='' /></div>
+                        <div className='p-3 flex items-center'>
+                            <div>
+                                <p className='text-sm'>Jan 24, 2022</p>
+                                <h1 className='text-xl font-bold my-2'>Stimulation Child's Sensory and Motor Development</h1>
+                                <p className='text-sm'>The objective of this study is to assess the findings of selected articles regarding the therapeutic...</p>
+                                <div className='flex gap-5 items-center mt-5'>
+                                    <div className="avatar">
+                                        <div className="lg:w-12 md:w-14 sm:w-16 w-14 rounded-full">
+                                            <img src="https://placeimg.com/192/192/people" alt='' />
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <h2 className='font-semibold '>Bithika Abhedancada</h2>
+                                        <p className='text-sm'>Study Power CEO</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="">
+                    <div class="card w-52  bg-pink-100 shadow-xl ml-10  ">
+                        <div class="card-body p-4 ">
+                            <h2 className=' text-center text-[14px] '>Join Membership</h2>
+                            <h1 className='text-[16px] font-bold'>Want to access our <br /> premium content?</h1>
+                            <p className='text-sm'>Sometimes features require a sort <br /> description.This can be detailed <br /> description </p>
+                            <button class="btn btn-secondary rounded-full text-[14px]">Register Now</button>
+                            <button class="btn btn-outline  rounded-full btn-primary">Button</button>
+                           
                             
                         </div>
                     </div>
