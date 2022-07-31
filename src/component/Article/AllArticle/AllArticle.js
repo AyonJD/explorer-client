1e26f26c6e13fe30b1e8d888e975079bb328bafd
Ayon Jodder <ayonjodder177@gmail.com>
2022-08-01 02:39:39 +0600

    Article fetched by pagination api

--- src/component/Article/AllArticle/AllArticle.js
+++ src/component/Article/AllArticle/AllArticle.js
@@ -1,77 +1,39 @@
-import React, { useEffect, useState } from "react";
-import ArticleCard from "../ArticleCard/ArticleCard";
-import ArticlePagination from "../ArticlePagination/ArticlePagination";
+import React, { useEffect, useState } from 'react';
+import './AllArticle.css';
 
 const AllArticle = () => {
-    const [allUsersCount, setAllUsersCount] = useState(0);
     const [pageCount, setPageCount] = useState(0);
-    const [pageSize, setPageSize] = useState("10");
-    const [allUsers, setAllUsers] = useState([]);
-    const [users, setUsers] = useState([]);
-    const [currentPage, setCurrentPage] = useState(1);
-    const [dataRange, setDataRange] = useState("");
-    const [searchQuery, setSearchQuery] = useState("");
-    const [loading, setLoading] = useState(false);
-
+    const [articleCount, setArticleCount] = useState(10);
+    const [pageNumber, setPageNumber] = useState(0);
+    const [articles, setArticles] = useState([]);
 
     useEffect(() => {
-
-        fetch(`https://react-smart-data-table.herokuapp.com/users`)
-            .then((res) => res.json())
-            .then((data) => {
-                setAllUsers(data);
-            });
-    }, [allUsersCount]);
-
-    useEffect(() => {
-        if (pageSize === "10") {
-            fetch(`https://react-smart-data-table.herokuapp.com/users/count`)
-                .then((res) => res.json())
-                .then((data) => {
-                    setPageCount(Math.ceil(data.count / 10));
-                    setAllUsersCount(data.count);
-                });
-        }
-    }, [pageCount, pageSize]);
+        fetch(`http://localhost:5000/blogs?page=${pageNumber}&count=${articleCount}`)
+            .then(res => res.json())
+            .then(data => {
+                setArticles(data);
+            }
+            )
+    }, [pageNumber, articleCount]);
 
     useEffect(() => {
-        fetch(`https://react-smart-data-table.herokuapp.com/users/${pageSize}/${currentPage}`)
-            .then((res) => res.json())
-            .then((data) => {
-                setUsers(data);
+        fetch('http://localhost:5000/blogs-count')
+            .then(res => res.json())
+            .then(data => {
+                const count = Math.ceil(data.count / 10);
+                setPageCount(count);
             })
-            .then(() => {
-                if (pageSize !== "10") {
-                    setPageCount(Math.ceil(allUsersCount / pageSize));
-                }
-            });
-    }, [currentPage, pageSize, pageCount, allUsersCount]);
+    }, []);
 
-
+    // console.log(articles);
 
     return (
-        <div className="my-5">
-            {/* <div className="text-center"> */}
-            <ArticlePagination
-                dataRange={dataRange}
-                searchQuery={searchQuery}
-                setSearchQuery={setSearchQuery}
-                pageCount={pageCount}
-                currentPage={currentPage}
-                setCurrentPage={setCurrentPage}
-                allUsersCount={allUsersCount}
-                pageSize={pageSize}
-                setPageSize={setPageSize}
-            ></ArticlePagination>
-            {/* </div> */}
-            <ArticleCard
-                users={users}
-                allUsers={allUsers}
-                setDataRange={setDataRange}
-                totalUsers={allUsersCount}
-                searchQuery={searchQuery}
-                loading={loading}
-            ></ArticleCard>
+        <div class="pagination_container container mx-auto">
+            <ul class="pagination">
+                {
+                    [...Array()]
+                }
+            </ul>
         </div>
     );
 };
