5fda61e2ae101971f1500aa0059dc98d38ed5d72
Armin Sultana <arminsultana556@gmail.com>
2022-07-31 16:43:26 +0600

    Pull ayon bracch code

--- src/component/Contact/Contact.js
+++ src/component/Contact/Contact.js
@@ -6,23 +6,6 @@ import {TbPhoneCall } from "react-icons/tb";
 const Contact = () => {
     return (
 
-<<<<<<< HEAD
-<section className="text-gray-600 body-font relative mid-container">
-                <div className="container px-5 py-24 mx-auto flex sm:flex-nowrap flex-wrap">
-                    <div className="lg:w-2/3 md:w-1/2 bg-gray-300 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
-                        <iframe width="100%" height="100%" class="absolute inset-0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d37144.03679128258!2d91.0921473!3d22.87236290000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3754a4f60180b351%3A0xaf35df91820c2a37!2sMaijdee!5e1!3m2!1sen!2sbd!4v1653831893740!5m2!1sen!2sbd"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-                        <div className="bg-white relative flex flex-wrap py-6 rounded shadow-md">
-                            <div className="lg:w-1/2 px-6">
-                                <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs">ADDRESS</h2>
-                                <p className="mt-1">Photo booth tattooed prism, portland taiyaki hoodie neutra typewriter</p>
-                            </div>
-                            <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
-                                <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs">EMAIL</h2>
-                                <a className="text-indigo-500 leading-relaxed"><MdOutgoingMail></MdOutgoingMail>explorer@gmail.com</a>
-                                <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs mt-4">PHONE</h2>
-                                <p className="leading-relaxed"><TbPhoneCall></TbPhoneCall>123-456-7890</p>
-                            </div>
-=======
 
         <section className="mid-container">
             <div className="flex w-full items-center ">
@@ -42,7 +25,6 @@ const Contact = () => {
                         <div className="lg:w-1/2 px-6">
                             <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs">ADDRESS</h2>
                             <p className="mt-1">Photo booth tattooed prism, portland taiyaki hoodie neutra typewriter</p>
->>>>>>> 6bd89f0487b86d018530449d1a4694b9bdbff7d9
                         </div>
                         <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
                             <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs">EMAIL</h2>
