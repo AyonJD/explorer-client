50de6cda24c13902ea2317ae0489045d43a903ec
Armin Sultana <arminsultana556@gmail.com>
2022-07-31 14:28:16 +0600

    modify Contact page

--- src/component/Contact/Contact.js
+++ src/component/Contact/Contact.js
@@ -21,10 +21,10 @@ const Contact = () => {
                             </div>
                         </div>
                     </div>
-                    <div className="lg:w-1/3 md:w-1/2 bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+                    <div className=" p-10 lg:w-2/4 md:w-1/2 bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
                         <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">Contact Us</h2>
                         <p className="leading-relaxed mb-5 text-gray-600">Post-ironic portland shabby chic echo park, banjo fashion axe</p>
-                        <div className="relative mb-4">
+                        <div className="relative p mb-4">
                             <label htmlFor="name" className="leading-7 text-sm text-gray-600">Name</label>
                             <input type="text" id="name" name="name" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                         </div>
@@ -36,7 +36,7 @@ const Contact = () => {
                             <label htmlFor="message" className="leading-7 text-sm text-gray-600">Message</label>
                             <textarea id="message" name="message" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out" defaultValue={""} />
                         </div>
-                        <button className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Send Message</button>
+                        <button className="text-white bg-orange-400 border-0 py-2 px-6 focus:outline-none rounded hover:text-warning text-lg">Send Message</button>
                         <p className="text-xs text-gray-500 mt-3">Chicharrones blog helvetica normcore iceland tousled brook viral artisan.</p>
                     </div>
                 </div>
