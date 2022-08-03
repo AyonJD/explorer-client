9384cf9cb8d82e66ecf4b9ae77be26a9c9e7b842
mk-hasan <mkhasan6891@gmail.com>
2022-08-03 22:30:47 +0600

    some design changed

--- src/component/PremiumArticles/PremiumArticles.js
+++ src/component/PremiumArticles/PremiumArticles.js
@@ -15,7 +15,8 @@ const PremiumArticles = () => {
     const slicePremiumArticles = premiumArticles.slice(0, noOfPremiumArticles)
     const loadMorePremiumArticles = () => {
         setnoOfPremiumArticles(noOfPremiumArticles + noOfPremiumArticles);
-    }
+    };
+    const [show, setShow] = useState(false);
     return (
         <div className='mid-container'>
             <div className='flex w-full items-center'>
