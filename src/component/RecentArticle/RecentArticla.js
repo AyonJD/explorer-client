94a5668d794962d946de8ab99b3e7911d993b751
mozammelhoq1 <mozammelhoq512@gmail.com>
2022-08-04 21:59:27 +0600

    some update

--- src/component/RecentArticle/RecentArticla.js
+++ src/component/RecentArticle/RecentArticla.js
@@ -10,7 +10,6 @@ const RecentArticla = () => {
   const { articles } = valueObj;
   const articleCopy = [...articles];
 
-<<<<<<< HEAD
   return (
     <section className="mid-container">
       <div className="flex w-full items-center lg:mt-16 md:mt-36 sm:mt-44 mt-16">
@@ -26,15 +25,15 @@ const RecentArticla = () => {
         <div className="lg:w-[70%] lg:pr-10 md:pr-5 lg:py-5 md:py-5 lg:border-r-[1px] md:border-r-[1px]">
           <div className=" grid lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-2 gap-7 lg:mb-0 md:mb-0 sm:mb-5 mb-5">
             {articleCopy
-              .reverse()
-              .slice(0, 2)
-              .map((article) => (
-                <ArticleItem key={article._id} article={article}></ArticleItem>
+              ?.reverse()
+              ?.slice(0, 2)
+              .map((article, index) => (
+                <ArticleItem key={index} article={article} />
               ))}
           </div>
 
           <div className="mt-5 grid lg:grid-cols-2 md:grid-cols-2 gap-x-7 gap-y-4">
-            {articles.slice(0, 6).map((article) => (
+            {articles?.slice(0, 6).map((article) => (
               <RecentArticleItems
                 key={article._id}
                 article={article}
@@ -61,56 +60,6 @@ const RecentArticla = () => {
       </div>
     </section>
   );
-=======
-
-    return (
-        <section className='mid-container'>
-            <div className='flex w-full items-center lg:mt-16 md:mt-36 sm:mt-44 mt-16'>
-                <h1 className='text-3xl font-bold lg:w-72 w-72 md:w-96 sm:w-96'>Recent Article</h1>
-                <span className='bg-primary h-[2px] w-full article-border'></span>
-            </div>
-            <p className='text-sm mt-2'>Recent articles are selected based on update published</p>
-            <div className="mt-10 lg:flex md:flex">
-                <div className='lg:w-[70%] lg:pr-10 md:pr-5 lg:py-5 md:py-5 lg:border-r-[1px] md:border-r-[1px]'>
-                    <div className=" grid lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-2 gap-7 lg:mb-0 md:mb-0 sm:mb-5 mb-5">
-                        {articleCopy?.reverse()?.slice(0, 2).map((article, index) => (
-
-                            < ArticleItem
-                                key={index}
-                                article={article}
-                            />
-                        ))}
-                    </div>
-
-                    <div className="mt-5 grid lg:grid-cols-2 md:grid-cols-2 gap-x-7 gap-y-4">
-                        {articles?.slice(0, 6).map((article) => (
-                            <RecentArticleItems
-                                key={article._id}
-                                article={article}
-                            ></RecentArticleItems>
-                        ))}
-                    </div>
-                </div>
-                <div className="lg:w-[30%] grid  lg:ml-5 md:ml-5 mt-10 lg:mt-0 md:mt-0">
-                    <div>
-                        <h2 className='bg-black text-white py-2 pl-3 rounded'>OUR PICKS</h2>
-                    </div>
-                    <div className=" grid">
-                        {articles.slice(0, 4).map((article) => (
-                            <RecentArticleRight
-                                key={article._id}
-                                article={article}
-                            ></RecentArticleRight>
-                        ))}
-                    </div>
-                    <div>
-                        <SocialLinked />
-                    </div>
-                </div>
-            </div>
-        </section >
-    );
->>>>>>> 2a33331d82915d859ddb432b1d8b141024cb54d0
 };
 
 export default RecentArticla;
