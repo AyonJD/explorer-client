0d8ff1a32fd6bd0e6126b08fc6fed7dbef5a43cb
Imran Hossen <freelancerimran81@gmail.com>
2022-08-04 20:06:10 +0600

    finally done All Article page design

--- src/component/RecentArticle/RecentArticleItems.js
+++ src/component/RecentArticle/RecentArticleItems.js
@@ -10,13 +10,13 @@ const RecentArticleItems = ({ article }) => {
 
     return (
         <div
-            className='flex items-center gap-3'
+            className='flex items-center gap-3 cursor-pointer'
             onClick={() => handleNavigate(article._id)}>
             <div className='w-[35%]'>
                 <img className='w-full h-20' src={article?.blogs?.img} alt={article?.blogs?.title} />
             </div>
             <div className='w-[65%]'>
-                <h2 className='text-sm font-semibold'>{article?.blogs?.Title.slice(0, 50)}</h2>
+                <h2 className='text-sm font-semibold hover:text-[#da334a]'>{article?.blogs?.Title.slice(0, 50)}</h2>
                 <p className='text-xs mt-2 font-light'>{article?.blogs?.date}</p>
             </div>
         </div>
