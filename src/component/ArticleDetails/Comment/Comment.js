3081e1f25311e576e413ddcd59ca23161ef76261
Imran Hossen <freelancerimran81@gmail.com>
2022-08-06 02:51:59 +0600

    Comment edit option set

--- src/component/ArticleDetails/Comment/Comment.js
+++ src/component/ArticleDetails/Comment/Comment.js
@@ -1,16 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Comment = ({ comment }) => {
+  const [hover, setHover] = useState(false)
   // console.log(comment?.author?.userInfo);
   return (
-    <div className="flex gap-3 mb-5">
+    <div  className="flex gap-3 mb-5">
       <div>
         <img className="rounded-full w-10 mt-1" src={comment?.author?.userInfo?.photoURL} alt="" />
       </div>
-      <div className="">
-        <div className="bg-neutral py-2 px-3 rounded-2xl text-warning">
-          <h6 className="font-bold text-sm cursor-pointer">{comment?.author?.userInfo?.name}</h6>
-          <p className="text-sm">{comment?.comment}</p>
+      <div >
+        <div onMouseLeave={() => setHover(false)} className="flex justify-between items-center gap-2">
+          <div onMouseEnter={() => setHover(true)} className="bg-neutral py-2 px-3 rounded-2xl text-warning ">
+            <h6 className="font-bold text-sm cursor-pointer">{comment?.author?.userInfo?.name}</h6>
+            <p className="text-sm">{comment?.comment}</p>
+          </div>
+          {
+            hover && <div onMouseLeave={() => setHover(false)} className="cursor-pointer text-xl font-semibold ">
+              ...
+            </div>
+          }
         </div>
         <div className="flex gap-4 text-sm font-semibold mt-1 ml-2">
           <h6 className="cursor-pointer">Like</h6>
