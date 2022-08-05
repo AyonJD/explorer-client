7198f47f1f3273f4fe209ffe576e0dbc51c3861a
Imran Hossen <freelancerimran81@gmail.com>
2022-08-06 00:25:13 +0600

    login form design practice

--- src/App.js
+++ src/App.js
@@ -23,6 +23,7 @@ import ScrollToTop from "./hooks/ScrollToTop";
 
 import { useSelector, useDispatch } from "react-redux";
 import getAllArticles from "./source/actions/articlesAction";
+import Hudai from "./Hudai";
 
 const articleDataContext = createContext();
 function App() {
@@ -125,6 +126,7 @@ function App() {
           <Route path="/register" element={<Register />}></Route>
           <Route path="/all-article" element={<AllArticle />}></Route>
           <Route path="/contact" element={<Contact />}></Route>
+          <Route path="/hudai" element={<Hudai />}></Route>
           <Route
             path="/article/:articleId"
             element={<ArticleDetails />}
