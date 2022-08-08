900e03c3813d29a1bd63a7190a4df7112a77de25
Imran Hossen <freelancerimran81@gmail.com>
2022-08-08 14:21:18 +0600

    user data get on the profile page

--- src/Dashboard/AdminDashboard/UserProfile/UserProfile.js
+++ src/Dashboard/AdminDashboard/UserProfile/UserProfile.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { articleDataContext } from '../../../App';
 import cover from '../../../assets/Profile/coverpic.jpg'
+import './UserProfile.css'
 
 
 const UserProfile = () => {
@@ -10,28 +11,43 @@ const UserProfile = () => {
     const userImg = signedInUser?.userInfo?.photoURL;
 
     return (
-        <div className='bg-[#F8F8F8]'>
-            <div className='bg-white w-[1200px] mx-auto'>
-                <div className="cover relative">
+        <div className='bg-[#F8F8F8] h-[100vh]'>
+            <div className='profile-container'>
+                <div className="cover relative  w-full">
                     <div>
-                        <img src={cover} alt="" />
+                        <img className='w-full' src={cover} alt="" />
                     </div>
-                    <div className='absolute bottom-[-100px] pl-10 flex items-center gap-7'>
+                    <div className='absolute bottom-[-115px] pl-10 flex items-center gap-7  w-full'>
                         <div className="profile w-36 rounded-full ring ring-white ring-offset-base-100 ring-offset-2">
                             <img className='w-full rounded-full' src={userImg} alt="" />
                         </div>
-                        <div className='mt-5'>
+                        <div className='mt-10  w-full'>
                             <h6 className='text-3xl font-extrabold'>{signedInUser?.userInfo?.name}</h6>
                             <p>Web Developer</p>
+                            <div className='mt-4 flex justify-between'>
+                                <div>
+                                    <button className='btn btn-sm mr-2'>Article</button>
+                                    <button className='btn btn-sm mr-2'>About</button>
+                                    <button className='btn btn-sm mr-2'>Photos</button>
+                                    <button className='btn btn-sm'>Followers</button>
+                                </div>
+                                <div className='pr-5'>
+                                    <button className='btn btn-sm'>Edit Profile</button>
+                                </div>
+                            </div>
                         </div>
+
                     </div>
                 </div>
-                <div className='mid-container'>
-
+                <div className='profile-container h-32 bg-white shadow-sm rounded-b-xl'/>
+                <div className='profile-container h-32 bg-white mt-10'>
+                    <div className='mt-5'>
+                        dsa
+                    </div>
                 </div>
             </div>
         </div>
-    );
+    ); 
 };
 
 export default UserProfile;
