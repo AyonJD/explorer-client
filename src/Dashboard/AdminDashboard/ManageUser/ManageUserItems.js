5a0e4578fe91512f3f241cd15a7fefc65d1c9e77
Ayon Jodder <ayonjodder177@gmail.com>
2022-08-11 12:54:02 +0600

    User section done in Dashboard

--- src/Dashboard/AdminDashboard/ManageUser/ManageUserItems.js
+++ src/Dashboard/AdminDashboard/ManageUser/ManageUserItems.js
@@ -1,14 +1,18 @@
 import React from 'react';
 
 const ManageUserItems = ({ user }) => {
-    console.log(user);
+    // console.log(user);
     const { admin, userInfo } = user;
+    const { email, name } = userInfo;
+    const membershipPlan = userInfo?.membershipPlan;
+
+    // console.log(membershipPlan)
     return (
         <tr>
-            <td className='py-5 pl-5'>Imran Hossen</td>
-            <td>imranhossen81@gmail.com</td>
-            <td>Admin</td>
-            <td>Premium</td>
+            <td className='py-5 pl-5'>{name}</td>
+            <td>{email}</td>
+            <td>{admin ? "Admin" : "User"}</td>
+            <td>{membershipPlan ? "Premium" : `${admin ? "All Access" : "Free"}`}</td>
             <td><button className='btn btn-neutral btn-xs text-warning'>Active</button></td>
             <td className='text-xl font-bold cursor-pointer'>...</td>
         </tr>
