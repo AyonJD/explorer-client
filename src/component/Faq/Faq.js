e02702eed0acd6587833543cbfd053cc7992338f
mk-hasan <mkhasan6891@gmail.com>
2022-08-11 17:45:44 +0600

    dynamically qusetion answer come in accordion

--- src/component/Faq/Faq.js
+++ src/component/Faq/Faq.js
@@ -8,39 +8,68 @@ const Faq = () => {
         title: "Payment",
         content: [
             {
-                question: "what is your payment?",
-                answer: "payment done."
+                question: "Does my subscription automatically renew??",
+                answer: "Pastry pudding cookie toffee bonbon jujubes jujubes powder topping. Jelly beans gummi bears sweet roll bonbon muffin liquorice. Wafer lollipop sesame snaps. Brownie macaroon cookie muffin cupcake candy caramels tiramisu. Oat cake chocolate cake sweet jelly-o brownie biscuit marzipan. Jujubes donut marzipan chocolate bar. Jujubes sugar plum jelly beans tiramisu icing cheesecake."
             },
             {
-                question: "what is your payment?",
-                answer: "payment done."
+                question: "Which license do I need to use an item in a commercial?",
+                answer: "At tempor commodo ullamcorper a lacus vestibulum. Ultrices neque ornare aenean euismod. Dui vivamus arcu felis bibendum. Turpis in eu mi bibendum neque egestas congue. Nullam ac tortor vitae purus faucibus ornare suspendisse sed."
             },
+            {
+                question: "Is the Regular License the same thing as an editorial license?",
+                answer: "Cheesecake muffin cupcake dragée lemon drops tiramisu cake gummies chocolate cake. Marshmallow tart croissant. Tart dessert tiramisu marzipan lollipop lemon drops. Cake bonbon bonbon gummi bears topping jelly beans brownie jujubes muffin. Donut croissant jelly-o cake marzipan. Liquorice marzipan cookie wafer tootsie roll. Tootsie roll sweet cupcake."
+            }
         ]
     };
     const titleDelivery = {
         title: "Delivery",
         content: [
             {
-                question: "what is your Delivery?",
-                answer: "Delivery done."
+                question: "Where has my order reached?",
+                answer: "Pastry pudding cookie toffee bonbon jujubes jujubes powder topping. Jelly beans gummi bears sweet roll bonbon muffin liquorice. Wafer lollipop sesame snaps. Brownie macaroon cookie muffin cupcake candy caramels tiramisu. Oat cake chocolate cake sweet jelly-o brownie biscuit marzipan. Jujubes donut marzipan chocolate bar. Jujubes sugar plum jelly beans tiramisu icing cheesecake."
             },
             {
-                question: "what is your Delivery?",
-                answer: "Delivery done."
+                question: "The shipment status shows that it has been returned/cancelled. What does it mean and who do I contact?",
+                answer: "Sweet pie candy jelly. Sesame snaps biscuit sugar plum. Sweet roll topping fruitcake. Caramels liquorice biscuit ice cream fruitcake cotton candy tart. Donut caramels gingerbread jelly-o gingerbread pudding. Gummi bears pastry marshmallow candy canes pie. Pie apple pie carrot cake."
             },
+            {
+                question: "What do I need to do to get the shipment delivered within a specific timeframe?",
+                answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+            }
         ]
     };
     const tileCancalation = {
         title: "Cancellation",
         content: [
             {
-                question: "what is your Cancellation?",
-                answer: "Cancellation done."
+                question: "How do I change my delivery address?",
+                answer: "Tart gummies dragée lollipop fruitcake pastry oat cake. Cookie jelly jelly macaroon icing jelly beans soufflé cake sweet. Macaroon sesame snaps cheesecake tart cake sugar plum. Dessert jelly-o sweet muffin chocolate candy pie tootsie roll marzipan."
             },
             {
-                question: "what is your Cancellation?",
-                answer: "Cancellation done."
+                question: "How can I cancel my shipment?",
+                answer: "Pastry pudding cookie toffee bonbon jujubes jujubes powder topping. Jelly beans gummi bears sweet roll bonbon muffin liquorice. Wafer lollipop sesame snaps. Brownie macaroon cookie muffin cupcake candy caramels tiramisu. Oat cake chocolate cake sweet jelly-o brownie biscuit marzipan. Jujubes donut marzipan chocolate bar. Jujubes sugar plum jelly beans tiramisu icing cheesecake."
             },
+            {
+                question: "What documents do I need to carry for self-collection of my shipment?",
+                answer: "At tempor commodo ullamcorper a lacus vestibulum. Ultrices neque ornare aenean euismod. Dui vivamus arcu felis bibendum. Turpis in eu mi bibendum neque egestas congue. Nullam ac tortor vitae purus faucibus ornare suspendisse sed. Commodo viverra maecenas accumsan lacus vel facilisis volutpat est velit. Tortor consequat id porta nibh. Id aliquet lectus proin nibh nisl condimentum id venenatis a. Faucibus nisl tincidunt eget nullam non nisi. Enim nunc faucibus a pellentesque. Pellentesque diam volutpat commodo sed egestas egestas fringilla phasellus. Nec nam aliquam sem et tortor consequat id. Fringilla est ullamcorper eget nulla facilisi. Morbi tristique senectus et netus et."
+            }
+        ]
+    }
+    const titleOrders = {
+        title: "Orders",
+        content: [
+            {
+                question: "How do I change my delivery address?",
+                answer: "Tart gummies dragée lollipop fruitcake pastry oat cake. Cookie jelly jelly macaroon icing jelly beans soufflé cake sweet. Macaroon sesame snaps cheesecake tart cake sugar plum. Dessert jelly-o sweet muffin chocolate candy pie tootsie roll marzipan."
+            },
+            {
+                question: "How can I cancel my shipment?",
+                answer: "Pastry pudding cookie toffee bonbon jujubes jujubes powder topping. Jelly beans gummi bears sweet roll bonbon muffin liquorice. Wafer lollipop sesame snaps. Brownie macaroon cookie muffin cupcake candy caramels tiramisu. Oat cake chocolate cake sweet jelly-o brownie biscuit marzipan. Jujubes donut marzipan chocolate bar. Jujubes sugar plum jelly beans tiramisu icing cheesecake."
+            },
+            {
+                question: "What documents do I need to carry for self-collection of my shipment?",
+                answer: "At tempor commodo ullamcorper a lacus vestibulum. Ultrices neque ornare aenean euismod. Dui vivamus arcu felis bibendum. Turpis in eu mi bibendum neque egestas congue. Nullam ac tortor vitae purus faucibus ornare suspendisse sed. Commodo viverra maecenas accumsan lacus vel facilisis volutpat est velit. Tortor consequat id porta nibh. Id aliquet lectus proin nibh nisl condimentum id venenatis a. Faucibus nisl tincidunt eget nullam non nisi. Enim nunc faucibus a pellentesque. Pellentesque diam volutpat commodo sed egestas egestas fringilla phasellus. Nec nam aliquam sem et tortor consequat id. Fringilla est ullamcorper eget nulla facilisi. Morbi tristique senectus et netus et."
+            }
         ]
     }
     const [active, setActive] = useState("first");
@@ -58,16 +87,16 @@ const Faq = () => {
             </div>
             <div className='flex mt-12 mid-container gap-10'>
                 <div className='w-1/3'>
-                    <button onClick={() => setActive('first')} className='btn py-2 w-full bg-neutral rounded-xl mb-3'>Delivery</button>
+                    <button onClick={() => setActive('first')} className='btn py-2 w-full bg-neutral rounded-xl mb-3'>Payment</button>
                     <button onClick={() => setActive('second')} className='btn py-2 w-full bg-neutral rounded-xl mb-3'>Cancellation</button>
-                    <button onClick={() => setActive('third')} className='btn py-2 w-full bg-neutral rounded-xl mb-3'>My Orders</button>
+                    <button onClick={() => setActive('third')} className='btn py-2 w-full bg-neutral rounded-xl mb-3'>Orders</button>
                     <button onClick={() => setActive('fourth')} className='btn py-2 w-full bg-neutral rounded-xl mb-3'>Services</button>
                 </div>
                 <div className='w-2/3 '>
-                    {active === "first" && <FaqRight title="1" />}
-                    {active === "second" && <FaqRight title="2" />}
-                    {active === "third" && <FaqRight title="3" />}
-                    {active === "fourth" && <FaqRight title="4" />}
+                    {active === "first" && <FaqRight titlePayment={titlePayment} />}
+                    {active === "second" && <FaqRight titleDelivery={titleDelivery} />}
+                    {active === "third" && <FaqRight tileCancalation={tileCancalation} />}
+                    {active === "fourth" && <FaqRight titleOrders={titleOrders} />}
                 </div>
             </div>
             <div className='mid-container'>
--- src/component/Faq/FaqRight.js
+++ src/component/Faq/FaqRight.js
@@ -1,25 +1,33 @@
 import React from 'react';
 
-const FaqRight = ({ title }) => {
+const FaqRight = ({ titlePayment }) => {
+    console.log(titlePayment);
     return (
         <div>
             <div>
-                <h4>Delivery</h4>
-                <p> {title} </p>
+                <h4>{titlePayment.title}</h4>
                 <div tabindex="0" class="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box mb-3">
                     <div class="collapse-title font-semibold">
-                        <h6>The shipment status shows that it has been returned/cancelled. What does it mean and who do I contact?</h6>
+                        <h6>{titlePayment.content[0].question}</h6>
                     </div>
                     <div class="collapse-content text-sm">
-                        <p>Sweet pie candy jelly. Sesame snaps biscuit sugar plum. Sweet roll topping fruitcake. Caramels liquorice biscuit ice cream fruitcake cotton candy tart. Donut caramels gingerbread jelly-o gingerbread pudding. Gummi bears pastry marshmallow candy canes pie. Pie apple pie carrot cake.</p>
+                        <p>{titlePayment.content[0].answer}</p>
                     </div>
                 </div>
                 <div tabindex="0" class="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box mb-3">
                     <div class="collapse-title font-semibold">
-                        <h6>The shipment status shows that it has been returned/cancelled. What does it mean and who do I contact?</h6>
+                        <h6>{titlePayment.content[1].question}</h6>
                     </div>
                     <div class="collapse-content text-sm">
-                        <p>Sweet pie candy jelly. Sesame snaps biscuit sugar plum. Sweet roll topping fruitcake. Caramels liquorice biscuit ice cream fruitcake cotton candy tart. Donut caramels gingerbread jelly-o gingerbread pudding. Gummi bears pastry marshmallow candy canes pie. Pie apple pie carrot cake.</p>
+                        <p>{titlePayment.content[1].answer}</p>
+                    </div>
+                </div>
+                <div tabindex="0" class="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box mb-3">
+                    <div class="collapse-title font-semibold">
+                        <h6>{titlePayment.content[2].question}</h6>
+                    </div>
+                    <div class="collapse-content text-sm">
+                        <p>{titlePayment.content[2].answer}</p>
                     </div>
                 </div>
             </div>
