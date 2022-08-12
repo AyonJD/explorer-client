2602c08940c815696e189847ecdbf20b12e950f3
Ayon Jodder <ayonjodder177@gmail.com>
2022-08-12 11:36:43 +0600

    Make admin functionality done

--- src/Dashboard/AdminDashboard/ManageUser/ManageUserItems.js
+++ src/Dashboard/AdminDashboard/ManageUser/ManageUserItems.js
@@ -1,25 +1,50 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Popup from 'reactjs-popup';
 import { useForm } from 'react-hook-form';
 import 'reactjs-popup/dist/index.css';
 import './ManageUserItems.css';
+import { articleDataContext } from '../../../App';
 
 const ManageUserItems = ({ user }) => {
     const { register, handleSubmit, formState: { errors }, trigger, reset } = useForm();
-    const { admin, userInfo } = user;
+    const valueObj = useContext(articleDataContext);
+    const { users } = valueObj;
+    const { admin, userInfo, _id } = user;
     const { email, name } = userInfo;
     const membershipPlan = userInfo?.membershipPlan;
+    const [refetchAdminRole, setRefetchAdminRole] = useState(false);
+
+
 
     const onSubmitParam = async data => {
-        
+        setRefetchAdminRole(true);
+
+        const user = {
+            admin: true,
+            userInfo: {
+                ...userInfo,
+                ocupation: data.possition,
+            }
+        }
+        //update user by put api
+        fetch(`http://localhost:5000/users/${_id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(user),
+        }).then((res) => res.json())
+            .then((data) => {
+                //reset form
+            });
         reset();
     }
     return (
         <tr>
             <td className='py-5 pl-5'>{name}</td>
             <td>{email}</td>
-            <td>{admin ? "Admin" : "User"}</td>
-            <td>{membershipPlan ? "Premium" : `${admin ? "All Access" : "Free"}`}</td>
+            <td>{admin || refetchAdminRole ? "Admin" : "User"}</td>
+            <td>{membershipPlan ? "Premium" : `${admin || refetchAdminRole ? "All Access" : "Free"}`}</td>
             <td><button className='btn btn-neutral btn-xs text-warning'>Active</button></td>
             <td className='text-xl font-bold cursor-pointer'>
                 <Popup className="popup_content" trigger={<button>...</button>} position="left center">
