c27f3d44b7bd6c0be7e9409880a28a4e79adffcf
Ayon Jodder <ayonjodder177@gmail.com>
2022-08-12 12:04:58 +0600

    Error

--- src/Dashboard/AdminDashboard/ManageUser/ManageUserItems.js
+++ src/Dashboard/AdminDashboard/ManageUser/ManageUserItems.js
@@ -5,6 +5,7 @@ import 'reactjs-popup/dist/index.css';
 import './ManageUserItems.css';
 import { articleDataContext } from '../../../App';
 import PopupForDeleteAdmin from './PopupForDeleteAdmin';
+import PopupForMakeAdmin from './PopupForMakeAdmin';
 
 const ManageUserItems = ({ user }) => {
     const { register, handleSubmit, formState: { errors }, trigger, reset } = useForm();
@@ -72,26 +73,8 @@ const ManageUserItems = ({ user }) => {
 
 
                 {
-                    admin || !refetchAdminRole ? (
-                        <Popup className="popup_content" trigger={<button>...</button>} position="left center">
-                            <form className='py-5 px-3' onSubmit={handleSubmit(onSubmitParam)}>
-                                <input type="text" placeholder='Admin possition' className="input h-8 w-full max-w-xs"
-                                    {...register("possition", {
-                                        required: true,
-                                        minLength: {
-                                            value: 3, message: 'Minimum 3 character required'
-                                        }
-                                    })}
-
-                                    onKeyUp={() => {
-                                        trigger('possition')
-                                    }}
-                                />
-                                <small className='text-[#FF4B2B] custom_font_size'>{errors?.name?.message}</small>
-
-                                <button className='btn btn-outline btn-primary btn-sm mt-5' type="submit">Make Admin</button>
-                            </form>
-                        </Popup>
+                    !admin || refetchAdminRole ? (
+                        <PopupForMakeAdmin onSubmitParam={onSubmitParam} />
                     )
                         : (
                             <PopupForDeleteAdmin handleDelete={handleDelete} />
