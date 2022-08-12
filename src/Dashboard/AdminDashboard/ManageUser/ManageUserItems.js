888328bfe7a2cc81dfa13ab3e9b2a0800d26dc32
Ayon Jodder <ayonjodder177@gmail.com>
2022-08-12 10:58:33 +0600

    Make admin popup done

--- src/Dashboard/AdminDashboard/ManageUser/ManageUserItems.js
+++ src/Dashboard/AdminDashboard/ManageUser/ManageUserItems.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import Popup from 'reactjs-popup';
+import { useForm } from 'react-hook-form';
+import 'reactjs-popup/dist/index.css';
+import './ManageUserItems.css';
 
 const ManageUserItems = ({ user }) => {
-    // console.log(user);
+    const { register, handleSubmit, formState: { errors }, trigger, reset } = useForm();
     const { admin, userInfo } = user;
     const { email, name } = userInfo;
     const membershipPlan = userInfo?.membershipPlan;
 
-    // console.log(membershipPlan)
+    const onSubmitParam = async data => {
+        
+        reset();
+    }
     return (
         <tr>
             <td className='py-5 pl-5'>{name}</td>
@@ -14,7 +21,28 @@ const ManageUserItems = ({ user }) => {
             <td>{admin ? "Admin" : "User"}</td>
             <td>{membershipPlan ? "Premium" : `${admin ? "All Access" : "Free"}`}</td>
             <td><button className='btn btn-neutral btn-xs text-warning'>Active</button></td>
-            <td className='text-xl font-bold cursor-pointer'>...</td>
+            <td className='text-xl font-bold cursor-pointer'>
+                <Popup className="popup_content" trigger={<button>...</button>} position="left center">
+                    <form className='py-5 px-3' onSubmit={handleSubmit(onSubmitParam)}>
+                        <input type="text" placeholder='Admin possition' className="input h-8 w-full max-w-xs"
+                            {...register("possition", {
+                                required: true,
+                                minLength: {
+                                    value: 3, message: 'Minimum 3 character required'
+                                }
+                            })}
+
+                            onKeyUp={() => {
+                                trigger('possition')
+                            }}
+                        />
+                        <small className='text-[#FF4B2B] custom_font_size'>{errors?.name?.message}</small>
+
+                        <button className='btn btn-outline btn-primary btn-sm mt-5' type="submit">Make Admin</button>
+                    </form>
+                </Popup>
+
+            </td>
         </tr>
     );
 };
