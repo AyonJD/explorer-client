ca7ee304d3c83a0febdcf9ed5dde16f8ea49bf04
mk-hasan <mkhasan6891@gmail.com>
2022-08-18 17:49:58 +0600

    analytics chart customized

--- src/Dashboard/AdminDashboard/Analytics/Analytics.js
+++ src/Dashboard/AdminDashboard/Analytics/Analytics.js
@@ -54,31 +54,33 @@ const Analytics = () => {
                 </div>
             </div >
 
-            <div style={{ width: '100%', height: 300 }}>
-                <ResponsiveContainer>
-                    <ComposedChart
-                        width={500}
-                        height={400}
-                        data={data}
-                        margin={{
-                            top: 20,
-                            right: 20,
-                            bottom: 20,
-                            left: 20,
-                        }}
-                    >
-                        <CartesianGrid stroke="#f5f5f5" />
-                        <XAxis dataKey="name" scale="band" />
-                        <YAxis />
-                        <Tooltip />
-                        <Legend />
-                        <Area type="monotone" dataKey="amt" fill="#8884d8" stroke="#8884d8" />
-                        <Bar dataKey="pv" barSize={20} fill="#413ea0" />
-                        <Line type="monotone" dataKey="uv" stroke="#ff7300" />
-                    </ComposedChart>
-                </ResponsiveContainer>
+            <div className='mt-4'>
+                <h4 className='font-bold text-xl'>User Liked</h4>
+                <div style={{ width: '100%', height: 300 }}>
+                    <ResponsiveContainer>
+                        <ComposedChart
+                            width={500}
+                            height={400}
+                            data={data}
+                            margin={{
+                                top: 20,
+                                right: 20,
+                                bottom: 20,
+                                left: 20,
+                            }}
+                        >
+                            <CartesianGrid stroke="#f5f5f5" />
+                            <XAxis dataKey="name" scale="band" />
+                            <YAxis />
+                            <Tooltip />
+                            <Legend />
+                            <Area type="monotone" dataKey="amt" fill="#8884d8" stroke="#8884d8" />
+                            <Bar dataKey="pv" barSize={20} fill="#413ea0" />
+                            <Line type="monotone" dataKey="uv" stroke="#ff7300" />
+                        </ComposedChart>
+                    </ResponsiveContainer>
+                </div>
             </div>
-
         </div >
     );
 };
