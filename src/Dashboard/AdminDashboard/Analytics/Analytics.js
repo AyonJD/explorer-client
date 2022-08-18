a4ea915658eefb826ae78dd62a982931d39e429f
mk-hasan <mkhasan6891@gmail.com>
2022-08-18 17:34:05 +0600

    analytics chart responsive

--- src/Dashboard/AdminDashboard/Analytics/Analytics.js
+++ src/Dashboard/AdminDashboard/Analytics/Analytics.js
@@ -3,78 +3,57 @@ import { PieChart, Pie, Sector, Cell, ResponsiveContainer, Tooltip } from 'recha
 
 const Analytics = () => {
     const data = [
-        {
-            "month": "Mar",
-            "investment": 100000,
-            "sell": 241,
-            "revenue": 10401
-        },
-        {
-            "month": "Apr",
-            "investment": 200000,
-            "sell": 423,
-            "revenue": 24500
-        },
-        {
-            "month": "May",
-            "investment": 500000,
-            "sell": 726,
-            "revenue": 67010
-        }
+        { name: 'Basic Article', value: 400 },
+        { name: 'Travel Article', value: 300 },
+        { name: 'Education Article', value: 300 },
+        { name: 'Premium Article', value: 200 },
     ];
-    const data2 = [
-        {
-            "month": "MarHalf",
-            "sell": 350
-        },
-        {
-            "month": "MarFull",
-            "sell": 285
-        },
-        {
-            "month": "AprHalf",
-            "sell": 430
-        },
-        {
-            "month": "AprFull",
-            "sell": 521
-        },
-        {
-            "month": "MayHalf",
-            "sell": 600
-        }
-    ];
+    const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+
+    const RADIAN = Math.PI / 180;
+    const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
+        const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+        const x = cx + radius * Math.cos(-midAngle * RADIAN);
+        const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+        return (
+            <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+                {`${(percent * 100).toFixed(0)}%`}
+            </text>
+        );
+    };
+
+    // export default class Example extends PureComponent {
+    //     static demoUrl = 'https://codesandbox.io/s/pie-chart-with-customized-label-dlhhj';
+
 
     return (
-        <div className="mid-container">
+        <div className="mid-container" >
             <div className='mt-4'>
-                <h5 className='text-center'>Articles Posted</h5>
-                <PieChart width={1000} height={600}>
-                    <Pie data={data} dataKey="investment" cx="50%" cy="50%" outerRadius={60} fill="#8884d8" />
-                    <Pie data={data2} dataKey="sell" cx="50%" cy="50%" innerRadius={70} outerRadius={90} fill="#82ca9d" label />
-                    <Tooltip></Tooltip>
-                </PieChart>
-            </div>
-
-            {/* <ResponsiveContainer width="100%" height="100%" >
-                <PieChart width={400} height={400}>
-                    <Pie
-                        data={data}
-                        cx="50%"
-                        cy="50%"
-                        labelLine={false}
-                        label={renderCustomizedLabel}
-                        outerRadius={80}
-                        fill="#8884d8"
-                        dataKey="value"
-                    >
-                        {data.map((entry, index) => (
-                            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-                        ))}
-                    </Pie>
-                </PieChart>
-            </ResponsiveContainer > */}
-        </div>
+                <h5 className='font-bold text-xl'>User Visited</h5>
+                <div style={{ width: '100%', height: 450 }}>
+                    <ResponsiveContainer>
+                        <PieChart>
+                            <Pie
+                                data={data}
+                                cx={200}
+                                cy={200}
+                                labelLine={false}
+                                label={renderCustomizedLabel}
+                                outerRadius={200}
+                                fill="#8884d8"
+                                dataKey="value"
+                            >
+                                {data.map((entry, index) => (
+                                    <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                                ))}
+                            </Pie>
+                            <Tooltip></Tooltip>
+                        </PieChart>
+                    </ResponsiveContainer>
+                </div>
+            </div >
+        </div >
     );
 };
 
