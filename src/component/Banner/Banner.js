7ee5df72d6415d96fc59fa76fbe7bf39ffa1314c
Md Imran Hossen <freelancerimran81@gmail.com>
2022-08-27 11:35:52 +0600

    banner section updated

--- src/component/Banner/Banner.js
+++ src/component/Banner/Banner.js
@@ -18,8 +18,22 @@ const Banner = () => {
 
     return (
         <section className='mid-container'>
-            <div className='flex gap-6'>
-                <div className='w-[30%] '>
+            <div className='md:flex flex gap-6'>
+            <div className='md:w-[60%] md:order-2 order-1'>
+                    <div className='mb-5'>
+                        {articles.slice(3, 4).map(article => {
+                            return (
+                                <div>
+                                    <img className='w-full' src={article?.blogs?.img} alt="" />
+                                    <h1 className='text-xl font-bold text-center mt-3'>{article?.blogs?.Title}</h1>
+                                    <p className='text-secondary text-center text-sm'>By- <span className='font-bold'>{article?.signedInUser?.userInfo?.name}</span></p>
+                                    <p className='text-secondary text-center px-3 mt-4'>By-{article?.blogs?.desc.slice(0, 200)}</p>
+                                </div>
+                            )
+                        })}
+                    </div>
+                </div>
+                <div className='md:w-[30%] w-[50%] md:order-1 '>
                     <div className='mb-5'>
                         {articles.slice(0, 1).map(article => {
                             return (
@@ -44,7 +58,7 @@ const Banner = () => {
                     </div>
                 </div>
 
-                <div className='w-[60%] '>
+                {/* <div className='w-[60%] md:order-2 order-1'>
                     <div className='mb-5'>
                         {articles.slice(3, 4).map(article => {
                             return (
@@ -57,9 +71,9 @@ const Banner = () => {
                             )
                         })}
                     </div>
-                </div>
+                </div> */}
 
-                <div className='w-[30%] '>
+                <div className='md:w-[30%] w-[50%] md:order-3 '>
                     <div className='mb-5'>
                         {articles.slice(5, 6).map(article => {
                             return (
