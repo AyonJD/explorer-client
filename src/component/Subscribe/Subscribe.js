41bb2fde7d1c8e9d2775fd6b74f4988212ce6706
Md Imran Hossen <freelancerimran81@gmail.com>
2022-08-29 16:30:10 +0600

    subscriber section responsive

--- src/component/Subscribe/Subscribe.js
+++ src/component/Subscribe/Subscribe.js
@@ -6,22 +6,22 @@ const Subscribe = () => {
     return (
         <div className='bg-accent'>
             <div className='mid-container' id='subscribe'>
-                <div className='grid grid-cols-2 gap-10 items-center'>
+                <div className='grid md:grid-cols-2 gap-10 items-center py-5 md:py-0'>
                     <div>
                         <h2 className='text-2xl font-semibold'>Subscribe to Updates</h2>
                         <p className='text-sm mt-3 mb-5'>Get the latest creative news from FooBar about art, design and business.</p>
                         <div className='flex items-center '>
-                            <input className=' p-3 w-full rounded-l border' placeholder='Your email address' type="" name="" value="" />
+                            <input className=' p-3 w-full rounded-l border focus:outline-none' placeholder='Your email address' type="" name="" value="" />
                             <button className='btn btn-primary rounded-l-none  rounded-r' type="">Subscribe</button>
                         </div>
-                        <div className='flex items-center gap-2 my-6'>
-                            <input type="checkbox" class="checkbox" /><p className='text-xs'>By signing up, you agree to the our terms and our Privacy Policy agreement</p>
+                        <div className='flex items-center gap-2 my-6 text-xs'>
+                            <input type="checkbox" class="checkbox w-5 h-5 rounded" /><p className=''>By signing up, you agree to the our terms and our Privacy Policy agreement</p>
                         </div>
-                        <img src={btn} alt="" />
+                        <img className='cursor-pointer' src={btn} alt="" />
                         <p className='text-xs mt-4'>Download HealthCare App. Its 100% FREE.</p>
                     </div>
 
-                    <div>
+                    <div className='md:block hidden'>
                         <img src={image} alt="" />
                     </div>
                 </div>
